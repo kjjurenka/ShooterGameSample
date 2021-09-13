2b5b582307a0e064ae976dd3363480553a9547b7
kjjurenka <56270768+kjjurenka@users.noreply.github.com>
2021-09-13 16:41:32 -0500

    Player Objects Created
    Setup player object
    Setup PlayerController Script

---
+++ ShooterGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+/***
+ * Created by: Kami Jurenka
+ * Date Created: 9/13/2021
+ * 
+ * Last Edited: 9/13/2021
+ * 
+ * Description: Player control movements
+ * **/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
